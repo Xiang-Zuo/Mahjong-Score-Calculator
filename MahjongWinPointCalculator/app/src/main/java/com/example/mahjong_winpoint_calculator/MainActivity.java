b0e1e25433577ffd62c8dfb2add31d7fa86168e5
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2020-04-28 13:15:01 -0400

    ignore .DS_Store file

--- MahjongWinPointCalculator/app/src/main/java/com/example/mahjong_winpoint_calculator/MainActivity.java
+++ MahjongWinPointCalculator/app/src/main/java/com/example/mahjong_winpoint_calculator/MainActivity.java
@@ -18,6 +18,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        // check camera permission
+
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
