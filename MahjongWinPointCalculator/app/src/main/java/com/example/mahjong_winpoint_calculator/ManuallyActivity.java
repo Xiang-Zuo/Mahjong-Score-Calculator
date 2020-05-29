2c3e9d8ce3a08fb93d675252f0abf7a0311dc305
XIANG ZUO <35638284+xiangzuo951218@users.noreply.github.com>
2020-05-28 21:12:34 -0400

    new card templates update

---
+++ MahjongWinPointCalculator/app/src/main/java/com/example/mahjong_winpoint_calculator/ManuallyActivity.java
@@ -0,0 +1,14 @@
+package com.example.mahjong_winpoint_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManuallyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manually);
+    }
+}
